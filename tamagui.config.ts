e4e4f3d71fc38e69279043e44183940c45457584
cyanideph <67945955+cyanideph@users.noreply.github.com>
2026-09-26 02:03:29 +0800

    Initialize X-App Bento foundation: tamagui.config.ts

---
+++ tamagui.config.ts
@@ -0,0 +1,11 @@
+import { createTamagui } from 'tamagui';
+import { defaultConfig } from '@tamagui/config/v5';
+
+export const tamaguiConfig = createTamagui(defaultConfig);
+export default tamaguiConfig;
+
+export type AppTamaguiConfig = typeof tamaguiConfig;
+
+declare module 'tamagui' {
+  interface TamaguiCustomConfig extends AppTamaguiConfig {}
+}
